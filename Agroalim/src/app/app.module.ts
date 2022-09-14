3a199a8a11420a4bd8b6cfa177e19e7cfd0f2588
Raul Carvajal <raul.crvjl.isc@outlook.com>
2022-09-14 09:59:55 -0500

    Se agregaron las liberias - Material Desing, Boostrap, JQuery, Popper.js

--- Agroalim/src/app/app.module.ts
+++ Agroalim/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RegistroComponent } from './registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
